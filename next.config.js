4eb778e4d40882b32ffa4c135122df9563fbfa57
juliajeske <julia.jeske@wehandle.com.br>
2023-03-23 18:54:27 -0300

    01- Setup & estrutura

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'], // arquivos que acabam com 'page.tsx' sera paginas
 }
 
 module.exports = nextConfig
