bf3158f8ef220138ce477259c683fd7231eb5704
juliajeske <julia.jeske@wehandle.com.br>
2023-04-07 15:12:17 -0300

    fix: avaivable.api code

--- src/pages/api/users/[username]/available.api.ts
+++ src/pages/api/users/[username]/available.api.ts
@@ -1,19 +1,22 @@
-import { prisma } from '@/lib/prisma'
 import dayjs from 'dayjs'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../../lib/prisma'
 
-export default async function handle(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   if (req.method !== 'GET') {
     return res.status(405).end()
   }
+
   const username = String(req.query.username)
   const { date } = req.query
+
   if (!date) {
-    return res.status(400).json({ message: 'Date not provided.' })
+    return res.status(400).json({ message: 'Date no provided.' })
   }
+
   const user = await prisma.user.findUnique({
     where: {
       username,
@@ -28,7 +31,7 @@ export default async function handle(
   const isPastDate = referenceDate.endOf('day').isBefore(new Date())
 
   if (isPastDate) {
-    return res.json({ possibleTimes: [], availability: [] })
+    return res.json({ possibleTimes: [], availableTimes: [] })
   }
 
   const userAvailability = await prisma.userTimeInterval.findFirst({
@@ -39,35 +42,40 @@ export default async function handle(
   })
 
   if (!userAvailability) {
-    return res.json({ possibleTimes: [], availability: [] })
+    return res.json({ possibleTimes: [], availableTimes: [] })
   }
 
   const { time_start_in_minutes, time_end_in_minutes } = userAvailability
 
-  const startHours = time_start_in_minutes / 60
-  const endHours = time_end_in_minutes / 60
+  const startHour = time_start_in_minutes / 60
+  const endHour = time_end_in_minutes / 60
 
-  const possibleTimes = Array.from({ length: endHours - startHours }).map(
+  const possibleTimes = Array.from({ length: endHour - startHour }).map(
     (_, i) => {
-      return startHours + i
+      return startHour + i
     },
   )
 
   const blockedTimes = await prisma.scheduling.findMany({
+    select: {
+      date: true,
+    },
     where: {
       user_id: user.id,
       date: {
-        gte: referenceDate.set('hour', startHours).toDate(),
-        lte: referenceDate.set('hour', endHours).toDate(),
+        gte: referenceDate.set('hour', startHour).toDate(),
+        lte: referenceDate.set('hour', endHour).toDate(),
       },
     },
   })
 
   const availableTimes = possibleTimes.filter((time) => {
-    const isTimeBlocked = !blockedTimes.some(
-      (blockedTimes) => blockedTimes.date.getHours() === time,
+    const isTimeBlocked = blockedTimes.some(
+      (blockedTime) => blockedTime.date.getHours() === time,
     )
+
     const isTimeInPast = referenceDate.set('hour', time).isBefore(new Date())
+
     return !isTimeBlocked && !isTimeInPast
   })
 
